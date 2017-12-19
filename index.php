6d215e8e33b734ea33daeaca840ccc83887ad934
Valentin DELAPRE <valentin.delapre@gmail.com>
2017-12-19 10:10:55 +0100

    création pages

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>L'Oison</title>
+  </head>
+  <body>
+
+  </body>
+</html>
