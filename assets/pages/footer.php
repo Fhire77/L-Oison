c7d4838d9a6e3fd2cc609a579d7dd30af15c176b
Valentin DELAPRE <valentin.delapre@gmail.com>
2017-12-19 16:37:24 +0100

    v2

--- assets/pages/footer.php
+++ assets/pages/footer.php
@@ -1,13 +1,13 @@
-
+s
   <div class="footer">
 
-      <a href="http://www.lavalette-tude-dronne.fr/"><img id="image2" src="assets/image/imgpartenaire2.jpg"></a>
-      <a href="https://www.nouvelle-aquitaine.fr/"><img id="image5" src="assets/image/imgpartenaire5.png"></a>
-      <a href="https://www.lacharente.fr/"><img id="image3" src="assets/image/imgpartenaire3.jpg"></a>
-      <a href="http://www.msadescharentes.fr/lfy"><img id="image4" src="assets/image/imgpartenaire4.jpg"></a>
-      <a href="http://www.caf.fr/allocataires/caf-de-la-charente/points-d-accueil/angouleme-siege-de-la-caf"><img id="image1" src="assets/image/imgpartenaire1.gif"></a>
-      <a href="http://www.sports.gouv.fr/"><img id="image6" src="assets/image/imgpartenaire6.jpg"></a>
-      <a href="http://www.cdg16.fr/"><img id="image7" src="assets/image/imgpartenaire7.jpg"></a>
-      <a href="http://www.montmoreau-saint-cybard.fr/la-commune/communaute-de-communes-du-montmorelien"><img id="image8" src="assets/image/imgpartenaire8.jpg"></a>
+      <a href="http://www.lavalette-tude-dronne.fr/"><img id="image2" src="assets/images/imgpartenaire2.jpg"></a>
+      <a href="https://www.nouvelle-aquitaine.fr/"><img id="image5" src="assets/images/imgpartenaire5.png"></a>
+      <a href="https://www.lacharente.fr/"><img id="image3" src="assets/images/imgpartenaire3.jpg"></a>
+      <a href="http://www.msadescharentes.fr/lfy"><img id="image4" src="assets/images/imgpartenaire4.jpg"></a>
+      <a href="http://www.caf.fr/allocataires/caf-de-la-charente/points-d-accueil/angouleme-siege-de-la-caf"><img id="image1" src="assets/images/imgpartenaire1.gif"></a>
+      <a href="http://www.sports.gouv.fr/"><img id="image6" src="assets/images/imgpartenaire6.jpg"></a>
+      <a href="http://www.cdg16.fr/"><img id="image7" src="assets/images/imgpartenaire7.jpg"></a>
+      <a href="http://www.montmoreau-saint-cybard.fr/la-commune/communaute-de-communes-du-montmorelien"><img id="image8" src="assets/images/imgpartenaire8.jpg"></a>
 
 </div>
