3db8fddb221aedbeda3654882d5efb8e768c7ffb
Valentin DELAPRE <valentin.delapre@gmail.com>
2017-12-19 15:26:41 +0100

    A mettre en page

--- index.php
+++ index.php
@@ -1,19 +1,45 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>L'Oison</title>
-  </head>
-  <body>
-<header>
-  <?php include("assets/pages/header.php") ?>
-</header>
-<article class="article">
-<?php include("assets/pages/article.php") ?>
-</article>
-<footer>
-  <?php include("assets/pages/footer.php") ?>
-</footer>
-  </body>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="assets/css/style.css">
+
+  <title>L'Oison</title>
+
+</head>
+
+<body>
+
+  <header>
+    <?php include("assets/pages/header.php") ?>
+  </header>
+
+  <div class="contenu">
+    <div class="container_article">
+
+      <article class="article1">
+        <?php include("assets/pages/article.php") ?>
+      </article>
+
+      <article class="article2">
+        <?php include("assets/pages/article2.php") ?>
+      </article>
+    </div>
+
+    <aside class="fb_news">
+
+      <div class="fb">
+
+      </div>
+      <div class="news">
+
+      </div>
+    </aside>
+  </div>
+
+  <footer>
+    <?php include("assets/pages/footer.php") ?>
+  </footer>
+
+</body>
 </html>
