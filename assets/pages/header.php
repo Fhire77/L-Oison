f85b0af59435f2aaef0016c91435ab7f3f70049c
Valentin DELAPRE <valentin.delapre@gmail.com>
2018-01-04 09:22:16 +0100

    tests menu

--- assets/pages/header.php
+++ assets/pages/header.php
@@ -1,13 +1,19 @@
 <div class="header">
-<div class="logo">
-  <img src="assets/images/L-Oison.jpg" alt="logo">
-</div>
+  <div class="logo">
+    <img src="assets/images/L-Oison.jpg" alt="logo">
+  </div>
 </div>
 <nav>
-  <ul>
+  <ul class="menu">
     <li>Evenements</li>
     <li>Ecole Multisports</li>
-    <li>Centre de Loisirs</li>
+    <li class="triger">Centre de Loisirs
+      <ul class="submenu">
+        <li>De 5 à 8 ans</li>
+        <li>De 8 à 11 ans</li>
+        <li>De 11 à 15 ans</li>
+      </ul>
+    </li>
     <li><a href="assets/pages/equipe.php">Notre Equipe</a></li>
   </ul>
 </nav>
---
+++ test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="test_style.css">
+    <title></title>
+  </head>
+  <body>
+    <a class="btn btn-navbar" id="btnMenu">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </a>
+
+  </body>
+</html>
